74e697db785b82473e01a75f8840c591d984621f
Lenostatos <Lenostatos@gmx.de>
2018-12-20 11:37:09 +0100

    experiment with most of the relevant GUI controls

--- Exam_project/GUI_Design/Main_Form.vb
+++ Exam_project/GUI_Design/Main_Form.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Main_Form
+    Private Sub Main_Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DateTimePicker2.CustomFormat = "yyyy-MM-dd HH:mm:ss"
+    End Sub
 
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
 End Class

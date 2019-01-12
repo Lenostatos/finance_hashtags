8318403aaa7392eea02db8bc18a9451207aa6956
Lenostatos <Lenostatos@gmx.de>
2019-01-12 15:43:21 +0100

    Follow the first steps of the MicroLite 'Getting Started' guide in order to configure the connection to the database.

--- Visual_Studio_Solution/Main_Application/Module_Main.vb
+++ Visual_Studio_Solution/Main_Application/Module_Main.vb
@@ -1,8 +1,14 @@
-﻿Module Module_Main
+﻿Imports MicroLite
+Imports MicroLite.Configuration
+
+Module Module_Main
 
     Sub Main()
 
-
+        Dim sessionFactory = New Configure
+        .Fluently()
+        .ForSQLiteConnection("SQLiteData")
+        .CreateSessionFactory();
 
     End Sub
 
